c755b39504d033fe8a4d6b96a2c514f937e664a2
Georgi Tangardzhiev <gkyshenko@abv.bg>
2025-01-31 13:07:27 +0200

    Visual Updates. Added Grad Descent to Math Section

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template_string, render_template, url_for, abort
+from flask import Flask, redirect, render_template_string, render_template, request, url_for, abort
 import nbformat
 from nbconvert import HTMLExporter
 
@@ -10,7 +10,8 @@ class Notebook:
         self.file = file
         self.is_open = "opened"
         
-notebooks = [Notebook("Simple Neural Network", "Simple-Neural-Network.ipynb")]
+ai_notebooks = [Notebook("Simple Neural Network", "Simple-Neural-Network.ipynb")]
+math_notebooks = [Notebook("Gradient Descent", "Grad_Descent.ipynb")]
     
     
 def render_notebook(file_name):
@@ -46,29 +47,33 @@ def contact():
 
 @app.route("/math")
 def math():
-    return render_template("math.html")
+    return render_template("math.html", notebooks=math_notebooks, current_url=request.path)
 
 @app.route("/ai")
 def ai():
-    return render_template("ai.html", notebooks=notebooks)
+    return render_template("ai.html", notebooks=ai_notebooks, current_url=request.path)
 
-@app.route("/notebook/<file>/<open>")
-def notebook(file, open):
+@app.route("/notebook/<file>/<open>/<path>")
+def notebook(file, open, path):
     notebook_content = render_notebook(file)
+    path = path
     if notebook_content:
-         return render_template("notebook.html", notebook_content=notebook_content, open=open)
+         return render_template("notebook.html", notebook_content=notebook_content, open=open, path=path)
     else:
         abort(404)
 
-@app.route("/close")
-def close_dialog():
-    return redirect(url_for("ai"))
+@app.route("/close/<url_path>")
+def close_dialog(url_path):
+    if url_path == "math":
+        return redirect(url_for("math"))
+    elif url_path == "ai":
+         return redirect(url_for("ai"))
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html'), 404
 
 if __name__ == '__main__':
-    app.run() 
+    app.run() #debug=True 
   
 
